36532ed9837018690e7d61d8b7fd3a1ba5c693fc
esteban1234 <tva_libra@hotmail.com>
2018-04-19 12:46:29 -0500

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="qqU3wxMnNe2TT3ADDcLzASPRumSH2eHMBN0Wp8d5spw" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>LIMPIEZA Y MANTENIMIENTO INTEGRAL OMKA</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
--- vistas/envia.php
+++ vistas/envia.php
@@ -22,8 +22,7 @@ $mail->From = $correo;
 $mail->FromName = $nombre;
 
 //To address and name
-// $mail->addAddress("info@limpiezaomka.com");
-$mail->addAddress("ballina.santiago@gmail.com");
+$mail->addAddress("info@limpiezaomka.com");
 
 
 $mail->isHTML(true);
